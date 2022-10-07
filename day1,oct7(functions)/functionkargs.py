1145d44a6c69555d44d3301af771eb0ea756676c
vetukuri-Tejo-varma <83815659+vetukuri-Tejo-varma@users.noreply.github.com>
2022-10-07 19:05:54 +0530

    day 1 oct 7 palle functions

---
+++ day1,oct7(functions)/functionkargs.py
@@ -0,0 +1,4 @@
+def m2(**a):
+    for i,j in a.items():
+        print(i,'=',j)
+m2(name="varma",phno=9704233357,bloodgroup="o+ve")
---
+++ day1,oct7(functions)/functions.py
@@ -0,0 +1,4 @@
+def m1(*a):
+    for i in a:
+        print(i)
+m1(1,2,3,4)
